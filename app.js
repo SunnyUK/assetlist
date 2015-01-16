994d14b7619ff6997881cb87b0d4653b6aca645c
Christoph Herrmann <developer@dragonprojects.de>
2015-01-16 02:06:21 +0100

    Add basic http authentication for all http requests on testserver

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ var config = {
         npm: {
             'dragonnodejs-webserver': {
                 app: { port: process.env.PORT },
+                auth: {
+                    realm: process.env.AUTH_REALM,
+                    user: process.env.AUTH_USER,
+                    password: process.env.AUTH_PASSWORD
+                },
                 bower: {
                     libraries: [],
                     path: __dirname + '/bower_components/'
